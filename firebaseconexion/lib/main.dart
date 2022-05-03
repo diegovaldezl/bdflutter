f00c939c3a1ffd08e6648717fc6db982cc902ccd
diegovaldezl <2547442017@mail.utec.edu.sv>
2022-05-02 20:46:31 -0600

    FInal

--- firebaseconexion/lib/main.dart
+++ firebaseconexion/lib/main.dart
@@ -19,7 +19,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Firebase',
+      title: 'Firebas',
       home: Principal(),
     );
   }
